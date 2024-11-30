4bd0b3d3023b41be20d2bec7cc32274ff2658a84
yuansheng1549 <yuansheng1515@icloud.com>
2024-11-30 17:35:47 +0800

    chore: enable source maps with shebang

--- src/cli.ts
+++ src/cli.ts
@@ -1,4 +1,4 @@
-#!/usr/bin/env node
+#!/usr/bin/env -S node --enable-source-maps
 import '#lib/setup';
 import { CommandStore } from '#lib/structures';
 import { rootCommand } from '#utils/rootCommand';
