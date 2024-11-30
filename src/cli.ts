5fd8194d114cbff924c699eef4a04ea76c4afedd
yuansheng1549 <yuansheng1515@icloud.com>
2024-11-30 17:51:22 +0800

    fix: manually construct a root data if shebang is used

--- src/cli.ts
+++ src/cli.ts
@@ -7,8 +7,11 @@ import { createColors } from 'colorette';
 
 createColors({ useColor: true });
 
+let rootData = getRootData().root;
+if (!rootData.endsWith('dist')) rootData = process.argv[1].slice(0, '/cli.js'.length);
+
 container.stores.register(new CommandStore());
-container.stores.registerPath(getRootData().root);
+container.stores.registerPath(rootData);
 
 await container.stores.load();
 
