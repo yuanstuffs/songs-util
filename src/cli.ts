d316274596b77ccd56a87554aa4a0cdc3d48249c
yuansheng1549 <yuansheng1515@icloud.com>
2024-11-30 17:54:04 +0800

    fix: replace the string instead

--- src/cli.ts
+++ src/cli.ts
@@ -8,7 +8,7 @@ import { createColors } from 'colorette';
 createColors({ useColor: true });
 
 let rootData = getRootData().root;
-if (!rootData.endsWith('dist')) rootData = process.argv[1].slice(0, '/cli.js'.length);
+if (!rootData.endsWith('dist')) rootData = process.argv[1].replace('/cli.js', '');
 
 container.stores.register(new CommandStore());
 container.stores.registerPath(rootData);
