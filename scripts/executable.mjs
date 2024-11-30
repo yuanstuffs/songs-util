bdf65297ea95bae46f606fb6e0027da63b12d77b
yuansheng1549 <yuansheng1515@icloud.com>
2024-11-30 17:06:00 +0800

    chore(scripts): fix permission error when used with `./dist/cli.js`

---
+++ scripts/executable.mjs
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+import { Result } from '@sapphire/result';
+import { access, chmod, constants } from 'node:fs/promises';
+
+if (process.platform !== 'linux') process.exit(0);
+
+const cli = new URL('../dist/cli.js', import.meta.url);
+const fileExists = await Result.fromAsync(() => access(cli, constants.F_OK));
+
+if (fileExists.isErr()) process.exit(0);
+
+await chmod(cli, 755); // Make cli file executable
+
+process.exit(0);
