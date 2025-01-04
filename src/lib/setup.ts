841b7c9e3ee5281bbabae174afea5258d4603c63
yuansheng1549 <yuansheng1515@icloud.com>
2025-01-05 02:58:46 +0800

    feat: color help menu

--- src/lib/setup.ts
+++ src/lib/setup.ts
@@ -1,3 +1,6 @@
 import { setup } from '@skyra/env-utilities';
+import * as colorette from 'colorette';
+
+colorette.createColors({ useColor: true });
 
 setup(new URL('../../src/.env', import.meta.url));
--- src/lib/utils/rootCommand.ts
+++ src/lib/utils/rootCommand.ts
@@ -1,6 +1,20 @@
 import { packageJson } from '#utils/constants';
+import { envParseString } from '@skyra/env-utilities';
+import * as colorette from 'colorette';
 import { Command } from 'commander';
 
 export const rootCommand = new Command() //
 	.command('songs-util')
-	.version(packageJson.version);
+	.description(
+		[
+			'Sync songs to another drive (/) or directory', //
+			`Loaded src directory: ${envParseString('SRC_DIR')}`
+		].join('\n')
+	)
+	.version(packageJson.version)
+	.configureHelp({
+		styleCommandDescription: (str) => colorette.cyanBright(str),
+		styleDescriptionText: (str) => colorette.dim(colorette.white(str)),
+		styleTitle: (title) => colorette.greenBright(title),
+		styleUsage: (str) => colorette.whiteBright(str)
+	});
